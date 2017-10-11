34decdd583d17eae41471f96670e43821a2a3766
Rujii <32706795+Rujii@users.noreply.github.com>
2017-10-11 15:31:16 +0300

    Initial commit

---
+++ mesaj2.php
@@ -0,0 +1,356 @@
+<?php
+	$nume=@$_POST["nume"];
+	$email=@$_POST["email"];
+	$mesaj=@$_POST["mesaj"];
+	
+	$text = "Nume: ".$nume."\n";
+	$text .= "Email: ".$email."\n";
+	$text .= "Mesaj: ".$mesaj."\n";
+	
+	$headers = 'From: noreplay@sra.ro' . "\r\n" .
+    'Reply-To: noreplay@sra.ro' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+	if(($sumai == $suma)&&($sumai > 0))
+	{
+		$err = 'Mesajul a fost trimis cu succes';
+		//mail("nikelton_bmw@yahoo.com", "Site istorie", $text, $headers);
+	}
+	else
+	{
+		$err = "Eroare";
+	}
+
+?>
+
+
+<!DOCTYPE html PUBLIC >
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title> Terorismul in lumea contemporana</title>
+<link href='http://fonts.googleapis.com/css?family=Droid+Serif' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link href="css/styles.css" rel="stylesheet" type="text/css" />
+</head>
+<body style="margin:0; padding:0">
+	<table width="100%" cellpadding="0" cellspacing="0" border="0">
+    	<tr bgcolor="171919" height="160">
+        	<td align="center" valign="bottom">
+            	<table  width="960px" border="0" height="70" bgcolor="d13f31">
+                	<div id="menu">
+                                <ul class="center">
+                                    <li ><a href="index.html">Acasa</a></li>
+                                    <li><a href="galerie.html">Galerie</a></li>
+                                    <li><a href="video.html">Video</a></li>
+                                    <li><a href="referate.html">referate</a></li> 
+                                    <li><a href="teste.html">Teste</a></li>
+                                    <li><a href="contact.html">Contact</a></li>
+                                </ul>
+                    </div>
+                </table>
+            </td>
+        </tr>
+    	<tr bgcolor="elece7">
+        	<td align="center">
+            	<table width="960" cellpadding="0" cellspacing="0" border="0">
+                	<tr>
+                    	<td>
+                        	<table width="100%" border="0" cellpadding="0" cellspacing="0">
+                            	<tr height="120">
+                                	<td width="70%" align="left">
+                                    	 <div class="logo">
+                                            <h1>Terorismul <span>in lumea contemporana</span></h1>
+                                         </div>
+                                    </td>
+                                    <td width="30%" align="right">
+                                    	<a href="https://www.facebook.com/pages/Direc%C8%9Bia-de-Investigare-a-Infrac%C8%9Biunilor-de-Criminalitate-Organizat%C4%83-%C8%99i-Terorism/349437378587248?fref=ts" target="_blank"><img src="images/facebook.png" style=" width:50px"></a>
+                                        <a href="https://twitter.com/?lang=ro" target="_blank"><img src="images/twitter.png" style="width:50px"></a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                    	<td>
+                            <table width="960" height="340" bgcolor="6F010E">
+                                <tr>
+                                    <td>
+                                       	<table>
+                                        	<tr>
+                                            	<td colspan="3" height="10"></td>
+                                        	</tr>
+                                        	<tr>
+                                            	<td width="10"></td>
+                                            	<td >
+                                                
+<script type="text/javascript" src="js/jquery-1.2.6.min.js"></script>
+
+<script type="text/javascript">
+function slideSwitch() {
+    var $active = $('#slideshow DIV.active');
+
+    if ( $active.length == 0 ) $active = $('#slideshow DIV:last');
+
+    var $next =  $active.next().length ? $active.next()
+        : $('#slideshow DIV:first');
+
+    $active.addClass('last-active');
+
+    $next.css({opacity: 0.0})
+        .addClass('active')
+        .animate({opacity: 1.0}, 1000, function() {
+            $active.removeClass('active last-active');
+        });
+}
+
+$(function() {
+    setInterval( "slideSwitch()", 5000 );
+});
+
+</script>
+
+<style type="text/css">
+#slideshow {
+    position:relative;
+    height: 273px;
+	width: 900px;
+}
+#slideshow DIV {
+    position:absolute;
+    top:0;
+    left:0;
+    z-index:8;
+    opacity:0.0;
+    height: 273px;
+	width: 900px;
+    background-color: inherit;
+}
+#slideshow DIV.active {
+    z-index:10;
+    opacity:1.0;
+}
+#slideshow DIV.last-active {
+    z-index:9;
+}
+#slideshow DIV IMG {
+    height: 273px;
+	width: 900px;
+    display: block;
+    border: 0;
+    margin-bottom: 0px;
+}
+</style>
+<div style="text-align:center">
+<table border="0" cellpadding="0" cellspacing="0" width="100%" height="100%">
+	<tr>
+    	<td align="center"> 
+            <div id="slideshow">
+                <div style="" class="active">
+                     <img src="images/s1.png" alt="s1.png" /></a>
+                </div>
+                <div style="">
+                     <img src="images/s2.png" alt="s2.png" /></a>
+                </div>
+                <div style="">
+                     <img src="images/s3.png" alt="s3.png" /></a>
+                </div>
+            </div>
+		</td>
+	</tr>
+</table>
+</div>
+                                        
+                                        
+                                        
+                                                                                  
+                                                </td>
+                                            	<td width="10"></td>
+                                        	</tr>
+                                        	<tr>
+                                            	<td colspan="3" height="10"></td>
+                                        	</tr>
+                                        </table>      
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                    	<td height="60"></td>
+                    </tr>
+                    <tr>
+                    	<td align="center">
+                        	<table width="960">
+                            	<tr>
+                                	<td>
+                                    	<table width="100%" bgcolor="#FFFFFF" cellpadding="0" cellspacing="0" border="0">
+                                        	<tr>
+                                            	<td height="20" colspan="3"></td>
+                                            </tr>
+                                        	<tr>
+                                            	<td width="20"></td>
+                                                <td width="650" bgcolor="#FFFFFF" style="text-align:justify">
+                                    	<h3 style="color:#2e3a54"></h3> 
+                                        <p> <a href=""><? echo $err;?></p>
+
+
+  
+                                        </td>
+                                            	<td width="20"></td>                                               
+                                            </tr>
+                                        	<tr>
+                                            	<td height="20" colspan="3"></td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                    <td width="20"></td>
+                                    <td valign="top">
+                                    	<table width="290">
+                                        	<tr>
+                                            <div class="side-bar">
+                                                    <div class="search">
+                                                      <div class="title">
+                                                        <h1>Cauta aici</h1>
+                                                      </div>
+                                                      <ul>
+                                                        <li>
+                                                          <input name="" type="text"  class="textfiled"/>
+                                                        </li>
+                                                        <li><a href="#"><img src="images/search-icon.jpg" alt="search" /></a></li>
+                                                      </ul>
+                                                    </div>
+                                              </div>
+                                            <br>
+                                            <br>
+                                            <br>
+                                            <br>
+                                            <br>
+                                            <br>
+                                            <br>
+                                            <br>
+                                            <div class="side-bar">
+                                            	<div class="side-bar .panel">
+                                            	<td bgcolor="6F010E" align="center" height="60">
+                                                	<a href="irak.html" align="center" style="color:#FFF">
+                                                		 Irak
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        	<tr>
+                                            	<td bgcolor="6F010E" align="center" height="60">
+                                                	<a href="afganistan.html" align="center" style="color:#FFF">
+                                                		 Afganistan
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        	<tr>
+                                            	<td bgcolor="6F010E" align="center" height="60">
+                                                	<a href="germania.html" align="center" style="color:#FFF">
+                                                		 Germania
+                                                    </a>
+                                                </td>
+                                            </tr>
+											<tr>
+                                            	<td bgcolor="6F010E" align="center" height="60">
+                                                	<a href="belgia.html" align="center" style="color:#FFF">
+                                                		 Belgia
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                            	<td bgcolor="6F010E" align="center" height="60">
+                                                	<a href="siria.html" align="center" style="color:#FFF">
+                                                		Siria
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                            	<td bgcolor="6F010E" align="center" height="60">
+                                                	<a href="turcia.html" align="center" style="color:#FFF">
+                                                		Turcia
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                            	<td bgcolor="6F010E" align="center" height="60">
+                                                	<a href="franta.html" align="center" style="color:#FFF">
+                                                		Franta
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                            	</div>
+                                            </div>
+                                        </table>
+                                    </td>
+                                </tr>
+                        	</table>
+                        </td>
+                    </tr>
+            	</table>
+            </td>
+        </tr>
+		<tr>
+                    <td height="60"></td>
+        </tr>
+        <tr>
+        	<td bgcolor="6F010E" height="120" align="center" style="color:#FFF">
+            	<table width="960" height="350" cellpadding="0" cellspacing="0" border="0" align="center">
+                	<tr>
+                    	<td width="600" valign="top">
+                        <div class="body">
+                        <br>
+					<form action="mesaj.php" method="post" id="message" class="section">
+						<p style="color:#FFF; font-size:23px;"><b>Contacteaza-ma</b></p>
+                        <br>
+						<ul>
+							
+								<label>Numele tau:</label>
+								<input type="text" value="" name="nume">
+							
+							
+								<label>Mail:</label>
+								<input type="text" value="" name="email">
+							
+							
+								<label>Mesaj:</label>
+								<textarea name="mesaj"></textarea>
+                                
+                                <label id="numere"></label>
+								<input type="text" value="" name="sumai" id="sumai" style="display:none">
+								<input type="text" value="" name="suma">
+                                
+							
+								<input type="submit" value="Trimite">
+							
+						</ul>
+					</form>
+                    </div>
+                    	</td>
+                        <td width="10"></td>
+                        <td width="480" valign="top">
+                        <br>
+                            <p style="color:#FFF; font-size:23px;"><b>Date de contact</b></p>
+                            <br>
+                            <p  style="color:#FFF"> Locatie : Sighisoara, Romania 545400</p>
+							<p  style="color:#FFF"> Telefon : 0748634039</p>
+                            <p  style="color:#FFF"> Email   : nikelton_bmw@yahoo.com </p>
+                           
+                        </td>
+                    </tr>
+                </table> 
+            	 © Copyright 2017. Toate drepturile rezervate.<br>Designed by Razvan Rujoi.
+                <br><br><br>
+            </td>
+        </tr>
+    </table>
+	<div style="z-index: 21; position:fixed; bottom:30px; right: 30px;">
+						<img src="images/up.png" onClick="window.scrollTo(0, 0);" style="cursor:pointer"/>
+	</div>
+   </body>
+</html>
+<script>
+	var x=Math.round(Math.random()*10);
+	var y=Math.round(Math.random()*10);
+	document.getElementById("numere").innerHTML=x+"+"+y;
+	documnet.getElementById("sumai").value=x+y;
+</script>
